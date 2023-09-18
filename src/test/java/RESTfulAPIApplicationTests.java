fad27cc8d7407fd13a6a7b8b3e9f5833174d11ef
karim <karim_amri@gmx.de>
2023-09-18 15:06:49 +0200

    Slight changes/refactorization, see for yourself.

--- src/test/java/RESTfulAPIApplicationTests.java
+++ src/test/java/RESTfulAPIApplicationTests.java
@@ -1,10 +1,7 @@
-package restapi;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
-import restapi.control.procake.InstanceController;
 
 import java.util.ArrayList;
 import java.util.HashMap;
