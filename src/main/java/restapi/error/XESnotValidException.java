79d189959eeb4baf2f49b4dba06f393461faedfb
karim <karim_amri@gmx.de>
2023-09-18 14:53:07 +0200

    - Only one schema will be used for validation of XES logs. This schema allows more than the one from IEEE.

--- src/main/java/restapi/error/XESnotValidException.java
+++ src/main/java/restapi/error/XESnotValidException.java
@@ -6,15 +6,14 @@ import restapi.service.DatabaseService;
  * Exception to be thrown if a given log is not valid against a given schema.
  */
 public class XESnotValidException extends RuntimeException{
-    public XESnotValidException(String log, DatabaseService.XSD_SCHEMATA.XSD xsd){
+    public XESnotValidException(String log){
         super(String.format(
                 """
                         The given log does not validate against schema.
-                        Schema: %s
                         Log:
                         %s""",
 
-                xsd.SOURCE,
+
                 log));
     }
 }
