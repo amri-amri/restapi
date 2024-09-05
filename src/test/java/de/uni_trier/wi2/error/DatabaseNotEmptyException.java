49bddded768e5ffc8ef70fba4ad1b80f088352ce
s6kaamri <s6kaamri@uni-trier.de>
2024-09-05 07:00:34 +0200

    testless

--- src/test/java/de/uni_trier/wi2/error/DatabaseNotEmptyException.java
+++
@@ -1,7 +0,0 @@
-package de.uni_trier.wi2.error;
-
-public class DatabaseNotEmptyException extends Exception {
-    public DatabaseNotEmptyException() {
-        super("Database has to be empty for this test");
-    }
-}
